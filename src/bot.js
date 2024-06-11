ec55a3ed74208f9b1368a8e1f683c328804cb2c6
Tien Hieu <122421124+nthpudding@users.noreply.github.com>
2024-06-12 01:47:24 +0700

    update bot.js and add handling file

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const {token} = process.env;
-const {client, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const fs = require('fs');
 
-const client = Client({ intents: GatewayIntentBits.Guilds});
+const client = new Client({ intents: GatewayIntentBits.Guilds});
 client.commands = new Collection();
+client.commandArray = [];
 
-const functionFolders = fs.readdirSync('./src/functions');
+const functionFolders = fs.readdirSync(`./src/functions`);
 for (const folder of functionFolders) {
     const functionFiles = fs
-        .readdirSync('./src/function/${folder}')
+        .readdirSync(`./src/functions/${folder}`)
         .filter((file) => file.endsWith(".js"));
     for (const file of functionFiles) {
-        require('./functions/${folder}/${file}')(client);
+        require(`./functions/${folder}/${file}`)(client);
     }
 }
+
+
+client.handleEvents();
+client.handleCommands();
+client.login(token);
