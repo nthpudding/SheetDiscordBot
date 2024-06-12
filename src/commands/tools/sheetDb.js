e166ac6b0313c61fd4f6b455f7045a5b0ad8f6f8
Tien Hieu <122421124+nthpudding@users.noreply.github.com>
2024-06-12 23:16:25 +0700

    SheetDB
    chore: Update npm dependencies in package.json

---
+++ src/commands/tools/sheetDb.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require("discord.js");
+const axios = require("axios");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("notelop8")
+    .setDescription("save data to sheet")
+    .addStringOption(option => option.setName('name').setDescription('This is a name').setRequired(true))
+    .addStringOption(option => option.setName('date').setDescription('This is a date').setRequired(true))
+    .addStringOption(option => option.setName('note').setDescription('This is a note').setRequired(true)),
+  async execute(interaction) {
+    await interaction.reply({
+      content: "Data added successfully!!!",
+    });
+
+    const Name = interaction.options.getString('Name')
+    const Date = interaction.options.getString('Date')
+    const Note = interaction.options.getString('Note')
+
+    try {
+      const response = await axios.post(
+        "https://sheetdb.io/api/v1/jsyzmpgotsu6j",
+        {
+          data: [
+            {
+              Name: `${Name}`,
+              Date: `${Date}`,
+              Note: `${Note}`
+            },
+          ],
+        }
+      );
+      console.log("Data added successfully:", response.data);
+    } catch (error) {
+      console.error("Error adding data to Google Sheet:", error.response.data);
+    }
+  },
+};
